3786819a8cbe3030d236dd63ccb7285505473ee7
heycn <x1chennan@qq.com>
2022-01-05 19:20:35 +0800

    aside css 1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -24,3 +24,22 @@
     components: { Topnav }
   };
 </script>
+
+<style lang="scss" scoped>
+  aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    > h2 {
+      margin-bottom: 4px;
+    }
+    > ol {
+      > li {
+        padding: 4px 0;
+      }
+    }
+  }
+</style>

cff2a3e4edd150e83027d3ef54d28a824955af81
heycn <x1chennan@qq.com>
2022-01-05 13:32:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>hi</div>
+  <heycn />
+</template>
+
+<script>
+  import heycn from './components/heycn.vue';
+  export default {
+    name: 'App',
+    components: {
+      heycn
+    }
+  };
+</script>

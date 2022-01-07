0f5d40d28ed3f4c57cc42993d83e7cace5953d17
heycn <x1chennan@qq.com>
2022-01-07 17:00:10 +0800

    支持 theme 属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -60,5 +60,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+
+  &.wheel-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.wheel-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>

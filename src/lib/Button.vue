8d7b6208bf76d0269d7f26b1d3bbeb4e0921af00
heycn <x1chennan@qq.com>
2022-01-07 15:30:41 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,7 +1,6 @@
 <template>
-  <button
-    class="wheel-button"
-    :class="`theme-${theme}`">
+  <button class="wheel-button"
+          :class="`theme-${theme}`">
     <slot/>
   </button>
 </template>
@@ -17,3 +16,49 @@ export default {
 };
 </script>
 
+<style lang="scss" scoped>
+$h: 32px;
+$border-color: #dcdee2;
+$color: #515a6e;
+$blue: #2d8cf0;
+$blue-light: #2d8cf0;
+$radius: 4px;
+.wheel-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: #f8f8f8;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover {
+    color: $blue-light;
+    border-color: $blue-light;
+
+  }
+
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
+</style>

ac1056339aaa18e96c0c91e5fc3da789834673e9
heycn <x1chennan@qq.com>
2022-01-09 18:33:49 +0800

    实现圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,6 +39,7 @@ export default {
 <style lang="scss" scoped>
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
 }
 
 .feature {

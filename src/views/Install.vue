3a9ae249bdde77548c787d95d813bd0b947434f3
heycn <x1chennan@qq.com>
2022-01-10 00:01:25 +0800

    支持markdown文件 锁死版本

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,13 +1,14 @@
 <template>
-  <article class="markdown-body">
-    <h1>安装</h1>
-    <p>打开终端运行下列命令：</p>
-    <pre><code>npm install king-ui</code></pre>
-    <p>或</p>
-    <pre><code>yarn add king-ui</code></pre>
-
-    <p>
-      下一节：<a href="#/doc/get-started">开始使用</a>
-    </p>
+  <article class="markdown-body" v-html="md">
   </article>
 </template>
+
+<script>
+import md from '../markdown/install.md'
+
+export default {
+  data() {
+    return {md}
+  }
+}
+</script>

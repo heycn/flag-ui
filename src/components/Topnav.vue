52b1cae51ca6bddd519adb51dfe89c7e1f8f53b7
heycn <x1chennan@qq.com>
2022-01-09 18:43:01 +0800

    优化文档路由

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -8,8 +8,9 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>

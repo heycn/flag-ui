7d4587eb49f263c415b24416c938e19af7305adc
heycn <x1chennan@qq.com>
2022-01-09 18:40:12 +0800

    features 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,15 +14,29 @@
       </div>
     </div>
     <div class="feature">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-happy"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-happy"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 
@@ -43,9 +57,46 @@ export default {
 }
 
 .feature {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+
+      > p {
+        grid-area: text
+      }
+    }
   }
 }
 

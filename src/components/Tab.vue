8529df43e7c9259fa8eb1be4f42f976c439c4318
heycn <x1chennan@qq.com>
2022-01-09 13:35:47 +0800

    路径写错了...

--- src/components/Tab.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    Tab 组件
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
---
+++ src/lib/Tab.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Tabs 组件
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss"></style>

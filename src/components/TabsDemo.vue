ad8a3ea66057e18a02a26a7c15a5c12832ff53d3
heycn <x1chennan@qq.com>
2022-01-09 14:56:13 +0800

    遇到BUG

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,7 +1,7 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
   </Tabs>
@@ -10,8 +10,13 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
 
 export default {
-  components: {Tabs, Tab}
+  components: {Tabs, Tab},
+  setup() {
+    const x = ref('导航1');
+    return {x};
+  }
 };
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="wheel-tabs">
     <div class="wheel-tabs-nav">
-      <div class="wheel-tabs-nav-item" v-for="(t,index) in titles" :key="index">
+      <div class="wheel-tabs-nav-item"
+           v-for="(t,index) in titles"
+           @click="select(t)"
+           :class="{selected: t === selected}"
+           :key="index">
         {{ t }}
       </div>
     </div>
     <div class="wheel-tabs-content">
-      <component class="wheel-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="wheel-tabs-content-item" :is="current"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
+import {computed} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -22,10 +32,18 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return {defaults, titles};
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    };
+    return {defaults, titles, current, select};
   }
 };
 </script>

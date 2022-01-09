e9e0f4f654bda4e9af842910887f40067c49fb02
heycn <x1chennan@qq.com>
2022-01-09 20:10:04 +0800

    填充文档内容

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    安装
-  </div>
-</template>
+  <article>
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install king-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add king-ui</code></pre>
 
-<script lang="ts">
-export default {};
-</script>
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
+</template>

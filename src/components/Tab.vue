11f1eb6b03badd4490bdabc768acd4ba9d081d8f
heycn <x1chennan@qq.com>
2022-01-09 13:27:11 +0800

    提交 BUG

---
+++ src/components/Tab.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    Tab 组件
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,13 +3,15 @@
     Tabs Demo
     <hr>
     <Tabs/>
+    <Tab/>
   </div>
 </template>
 
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
 
 export default {
-  components: {Tabs}
+  components: {Tabs, Tab}
 };
 </script>

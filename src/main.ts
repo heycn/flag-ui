b674006d05e12bcccd231aaed8db97994e60072c
heycn <x1chennan@qq.com>
2022-01-10 00:11:51 +0800

    优化 markdown 配置文件有问题 保留bug

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import './lib/wheel.scss';
+import './lib/gulu.scss';
 import './index.scss';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {router} from './router';
 import 'github-markdown-css';
+import Markdown from './components/Markdown.vue';
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+app.component('Markdown', Markdown);
--- src/router.ts
+++ src/router.ts
@@ -6,11 +6,12 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
+
+import {h} from 'vue';
+import Markdown from './components/Markdown.vue';
 
 const history = createWebHashHistory();
+const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
 export const router = createRouter({
   history: history,
   routes: [
@@ -20,14 +21,14 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: '', component: DocDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-started', component: GetStarted},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-started', component: md('get-started')},
+        {path: 'install', component: md('install')},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
-        {path: 'tabs', component: TabsDemo}
-      ]
-    }
-  ]
+        {path: 'tabs', component: TabsDemo},
+      ],
+    },
+  ],
 });

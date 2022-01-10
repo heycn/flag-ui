2071f32709357329ac3b4be19a284ef55a144f06
heycn <x1chennan@qq.com>
2022-01-10 23:19:26 +0800

    不适用最新语法

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -42,12 +42,12 @@ export default {
       }
     };
     const ok = () => {
-      if (props.ok?.() !== false) {
+      if (props.ok && props.ok() !== false) {
         close();
       }
     };
     const cancel = () => {
-      props.cancel?.();
+      props.cancel && props.cancel();
       close();
     };
     return {close, onClickOverlay, ok, cancel};

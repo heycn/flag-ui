857931b47a46664adff6097345f40920f143df9b
heycn <x1chennan@qq.com>
2022-01-10 17:37:47 +0800

    展示源代码并且高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch1Demo.__sourceCode }}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"/>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"/>
       </div>
     </div>
   </div>
@@ -32,13 +32,19 @@
 import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue';
 import Switch2Demo from './Switch2.demo.vue';
+import 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
 import {ref} from 'vue';
+
+// Prism 源代码是指向全局 但是TS不认识，强制把 Prism 定义在 window，骗过TS
+const Prism = (window as any).Prism;
 
 export default {
   components: {Button},
   setup() {
     const bool = ref(false);
-    return {bool, Switch1Demo, Switch2Demo};
+    return {bool, Switch1Demo, Switch2Demo, Prism}
+      ;
   }
 };
 </script>

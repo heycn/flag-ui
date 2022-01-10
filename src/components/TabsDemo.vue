808f54f31dd7e4958d37b2777bfa4ae1b8c37998
heycn <x1chennan@qq.com>
2022-01-10 20:41:31 +0800

    改造 Tabs demo

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>Tabs 示例</div>
-  <h1>示例1</h1>
-  <Tabs v-model:selected="x">
-    <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
-  </Tabs>
+  <h1>Tabs 示例</h1>
+  <Demo :component="Tabs1Demo"/>
 </template>
 
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue';
-import Tab from '../lib/Tab.vue';
-import {ref} from 'vue';
+import Demo from './Demo.vue';
+import Tabs1Demo from './Tabs1.demo.vue';
 
 export default {
-  components: {Tabs, Tab},
+  components: {
+    Demo
+  },
   setup() {
-    const x = ref('导航1');
-    return {x};
+    return {
+      Tabs1Demo
+    };
   }
 };
 </script>

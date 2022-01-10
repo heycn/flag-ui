3b54abc06b3cdf05d7271a411567b9be3099b872
heycn <x1chennan@qq.com>
2022-01-10 19:10:15 +0800

    三æ¬删掉无用的代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,22 +7,14 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue';
 import Switch1Demo from './Switch1.demo.vue';
 import Switch2Demo from './Switch2.demo.vue';
-import 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
-
-// Prism 源代码是指向全局 但是TS不认识，强制把 Prism 定义在 window，骗过TS
-const Prism = (window as any).Prism;
 import Demo from './Demo.vue';
-import {ref} from 'vue';
 
 export default {
-  components: {Button, Demo},
+  components: {Demo},
   setup() {
-    const bool = ref(false);
-    return {bool, Switch1Demo, Switch2Demo, Prism};
+    return {Switch1Demo, Switch2Demo};
   }
 };
 </script>

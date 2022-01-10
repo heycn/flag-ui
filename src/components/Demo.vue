6ea62677e42099cc5ad0c82c03074b87f7ba031c
heycn <x1chennan@qq.com>
2022-01-10 19:00:06 +0800

    优化 Demo you有bug了

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,24 +5,34 @@
       <component :is="component"/>
     </div>
     <div class="demo-actions">
-      <Button>查看代码</Button>
+      <Button @click="toggleCode">查看代码</Button>
     </div>
-    <div class="demo-code">
-      <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"/>
+    <div class="demo-code" v-if="codeVisible">
+      <pre class="language-html" v-html="html"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
 import 'prismjs';
 import 'prismjs/themes/prism-tomorrow.css';
+import {computed, ref} from 'vue';
 
 // Prism 源代码是指向全局 但是TS不认识，强制把 Prism 定义在 window，骗过TS
 const Prism = (window as any).Prism;
 export default {
+  components: {Button},
   props: {component: Object},
-  setup() {
-    return {Prism};
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(props.component.__sourceCode, Prism.languages.html, 'html');
+    });
+    const toggleCode = () => {
+      codeVisible.value = !codeVisible;
+    };
+    const codeVisible = ref(false);
+    return {Prism, html, codeVisible, toggleCode};
   }
 };
 </script>
